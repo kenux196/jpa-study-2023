20b5907c599e4582a018dd4838187f325e50057a
kenux196 <kenux.yun@gmail.com>
2023-02-13 19:59:49 +0900

    add paging

--- src/test/java/study/kenux/jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/kenux/jpa/repository/MemberRepositoryTest.java
@@ -1,24 +1,24 @@
 package study.kenux.jpa.repository;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.annotations.BatchSize;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.transaction.annotation.Transactional;
 import study.kenux.jpa.domain.Member;
 
-import java.time.Instant;
-import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -94,11 +94,19 @@ class MemberRepositoryTest {
     }
 
     @Test
-    void bulkSave() {
-        final long start = System.currentTimeMillis();
+    void bulkSaveTest() {
+        final int savedSize = bulkSave();
+
+        final List<Member> result = memberRepository.findAll();
+        assertThat(result).hasSize(savedSize);
+        log.info("member(0) = {}", result.get(0));
+    }
+
+    private int bulkSave() {
         final String query = "insert into member (name, age) " +
                 "values (?, ?)";
         final List<Member> memberList = createMemberList(10000);
+        final long start = System.currentTimeMillis();
         jdbcTemplate.batchUpdate(query, memberList, 100, (ps, argument) -> {
             ps.setString(1, argument.getName());
             ps.setInt(2, argument.getAge());
@@ -106,9 +114,19 @@ class MemberRepositoryTest {
         log.info("time = {}", System.currentTimeMillis() - start);
         em.clear();
 
-        final List<Member> result = memberRepository.findAll();
-        assertThat(result).hasSize(memberList.size());
-        log.info("member(0) = {}", result.get(0));
+        return memberList.size();
+    }
+
+    @Test
+    void paging() throws JsonProcessingException {
+        final int savedSize = bulkSave();
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("id").ascending());
+//        final PageRequest pageRequest = PageRequest.of(0, 10);
+        final Page<Member> pagedResult = memberRepository.findAll(pageable);
+        log.info("result = {}", pagedResult);
+        ObjectMapper objectMapper = new ObjectMapper();
+        final String json = objectMapper.writeValueAsString(pagedResult);
+        log.info("result json = {}", json);
     }
 
     private List<Member> createMemberList(int count) {
