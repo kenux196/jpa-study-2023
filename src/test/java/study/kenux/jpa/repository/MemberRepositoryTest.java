4669995b02146ce4e4b470c20eb6a65c3af67ba3
kenux196 <kenux.yun@gmail.com>
2023-02-13 19:23:56 +0900

    test

--- src/test/java/study/kenux/jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/kenux/jpa/repository/MemberRepositoryTest.java
@@ -3,18 +3,24 @@ package study.kenux.jpa.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.BatchSize;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.kenux.jpa.domain.Member;
 
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@DataJpaTest
-@SpringBootTest
+@DataJpaTest
+//@SpringBootTest
 @Slf4j
 class MemberRepositoryTest {
 
@@ -26,7 +32,6 @@ class MemberRepositoryTest {
 
 
     @Test
-    @Transactional
     void memberSave() {
         log.info("start-------------");
         final Member member = new Member("member1", 11);
@@ -56,7 +61,6 @@ class MemberRepositoryTest {
     }
 
     @Test
-    @Transactional
     void detachedEntityIsNotUpdate() {
         final Member member = new Member("member1", 11);
         memberRepository.save(member);
@@ -74,5 +78,23 @@ class MemberRepositoryTest {
         return entity.getId() != null  // must not be transient
                 && !em.contains(entity)  // must not be managed now
                 && em.find(Member.class, entity.getId()) != null;  // must not have been removed
+    }
+
+    @Test
+    void saveAll() {
+        final long start = System.currentTimeMillis();
+        final List<Member> memberList = createMemberList(1000);
+        memberRepository.saveAll(memberList);
+        final Instant end = Instant.now();
+        log.info("time = {}", System.currentTimeMillis() - start);
+    }
+
+    private List<Member> createMemberList(int count) {
+        List<Member> members = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            final Member member = new Member("member" + i, i + 10);
+            members.add(member);
+        }
+        return members;
     }
 }
