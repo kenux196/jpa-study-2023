15382a1c0921a95d5654cc248b5ee0322ee0aeef
skyun <skyun@a2tec.co.kr>
2023-07-26 21:30:32 +0900

    Add Test

--- src/main/java/study/kenux/jpa/test/BoardDataGenerator.java
+++ src/main/java/study/kenux/jpa/test/BoardDataGenerator.java
@@ -5,12 +5,16 @@ import study.kenux.jpa.domain.Board;
 import study.kenux.jpa.domain.Member;
 
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.UUID;
 
 public class BoardDataGenerator extends DataGenerator {
 
-    public static final int INIT_DATA_COUNT = 1000;
+    public static final int BOARD_TEST_DATA_COUNT = 1000;
+
+    private static OffsetDateTime BASE_DATE_TIME = OffsetDateTime.of(
+            2023, 1, 1, 1, 1, 1, 0, ZoneOffset.UTC);
 
     public BoardDataGenerator(EntityManager em) {
         super(em);
@@ -21,12 +25,12 @@ public class BoardDataGenerator extends DataGenerator {
         final List<Member> members = getMembers();
         final int memberCount = members.size();
 
-        for (int i = 0; i < INIT_DATA_COUNT; i++) {
+        for (int i = 0; i < BOARD_TEST_DATA_COUNT; i++) {
             Board board = Board.builder()
                     .title("Board Title-" + i)
                     .contents(UUID.randomUUID().toString())
-                    .createdDate(OffsetDateTime.now())
-                    .modifiedDate(OffsetDateTime.now())
+                    .createdDate(BASE_DATE_TIME.plusHours(i))
+                    .modifiedDate(BASE_DATE_TIME.plusHours(i + 1))
                     .member(members.get(i % memberCount))
                     .build();
             em.persist(board);
--- src/test/java/study/kenux/jpa/repository/BoardRepositoryTest.java
+++ src/test/java/study/kenux/jpa/repository/BoardRepositoryTest.java
@@ -2,13 +2,16 @@ package study.kenux.jpa.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import study.kenux.jpa.domain.Board;
-import study.kenux.jpa.test.BoardDataGenerator;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static study.kenux.jpa.test.BoardDataGenerator.*;
 
 
 class BoardRepositoryTest extends JpaRepositoryTest {
@@ -16,10 +19,48 @@ class BoardRepositoryTest extends JpaRepositoryTest {
     @Autowired
     private BoardRepository repository;
 
+    private final int page = 0;
+    private final int size = 10;
+
     @Test
-    void findAll() {
+    void testFindAll() {
         final List<Board> boardList = repository.findAll();
-        assertThat(boardList).hasSize(BoardDataGenerator.INIT_DATA_COUNT);
+        assertThat(boardList).hasSize(BOARD_TEST_DATA_COUNT);
         System.out.println("boardList.get(0) = " + boardList.get(0));
+    }
+
+    @Test
+    void testFindAllWithPage() {
+        final PageRequest pageable = PageRequest.of(page, size);
+        final Page<Board> result = getPagedBoard(pageable);
+
+        assertThat(result.getSize()).isEqualTo(10);
+        assertThat(result.getTotalPages()).isEqualTo(BOARD_TEST_DATA_COUNT / size);
+        assertThat(result.getTotalElements()).isEqualTo(BOARD_TEST_DATA_COUNT);
+
+        System.out.println("result = " + result);
+        display(result.getContent());
+    }
+
+    @Test
+    void testFindAllWithPageAndSort() {
+        Sort sort = Sort.by(
+                Sort.Order.asc("member"),
+                Sort.Order.asc("createdDate"),
+                Sort.Order.desc("title")
+        );
+        final PageRequest pageable = PageRequest.of(page, size, sort);
+        final Page<Board> result = getPagedBoard(pageable);
+
+        System.out.println("result = " + result);
+        display(result.getContent());
+    }
+
+    private Page<Board> getPagedBoard(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    private void display(List<Board> boards) {
+        boards.forEach(board -> System.out.println("board = " + board));
     }
 }
