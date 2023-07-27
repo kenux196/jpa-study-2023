271c2a286e3314c70203d8b4594570788b54131d
skyun <skyun@a2tec.co.kr>
2023-07-27 22:39:57 +0900

    modify BoardService

--- src/main/java/study/kenux/jpa/service/BoardService.java
+++ src/main/java/study/kenux/jpa/service/BoardService.java
@@ -5,8 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import study.kenux.jpa.repository.BoardJdbcTemplateRepository;
 import study.kenux.jpa.repository.BoardRepository;
+import study.kenux.jpa.repository.JdbcTemplateBoardRepository;
 import study.kenux.jpa.service.dto.BoardInfo;
 
 import java.util.List;
@@ -17,10 +17,10 @@ import java.util.List;
 public class BoardService {
 
     private final BoardRepository boardRepository;
-    private final BoardJdbcTemplateRepository boardJdbcTemplateRepository;
+    private final JdbcTemplateBoardRepository jdbcTemplateBoardRepository;
 
     public List<BoardInfo> getBoards() {
-        return boardJdbcTemplateRepository.findAllBoard();
+        return jdbcTemplateBoardRepository.findAllBoard();
     }
 
     public Page<BoardInfo> getBoards(Pageable pageable) {
