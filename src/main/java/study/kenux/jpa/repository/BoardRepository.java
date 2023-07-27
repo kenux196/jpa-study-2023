a47fcad1bce25f70ec252ed47a5675ba0c5d392b
kenux196 <kenux.yun@gmail.com>
2023-07-27 18:32:38 +0900

    Add test

--- src/main/java/study/kenux/jpa/repository/BoardRepository.java
+++ src/main/java/study/kenux/jpa/repository/BoardRepository.java
@@ -1,7 +1,13 @@
 package study.kenux.jpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import study.kenux.jpa.domain.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @Query("select b from Board b")
+    Page<Board> findAllPaged(Pageable pageable);
 }
